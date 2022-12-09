1ac0d6adba2c67b4130e88163faf9e0180cb98c2
oakarakum <onur.ardakarakum@outlook.com>
2022-12-09 13:12:32 +0300

    her birisi için ayrı textformfield oluşturuldu

--- lib/controller/contact_controller_data.dart
+++ lib/controller/contact_controller_data.dart
@@ -3,8 +3,8 @@ import 'package:getx_ex2/models/contact_model.dart';
 
 class AppData extends GetxController {
   var contacts = [].obs;
-  /* bunun https://stackoverflow.com/questions/64544571/flutter-getx-forms-validation validation'ı
-   RxString username = RxString('');
+  // bunun https://stackoverflow.com/questions/64544571/flutter-getx-forms-validation validation'ı
+  RxString username = RxString('');
   RxnString errorText = RxnString(null);
   Rxn<Function()> submitFunc = Rxn<Function()>(null);
 
@@ -58,7 +58,7 @@ class AppData extends GetxController {
           const Duration(seconds: 1), () => print('User account created'));
       return true;
     };
-  } */
+  }
 
   addContacts(ContactModel contact) {
     contacts.add(contact);
--- lib/page/contact_book_screen.dart
+++ lib/page/contact_book_screen.dart
@@ -19,68 +19,20 @@ class ContactBook extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(height: 10.h),
-          TextFormFieldWidget(
+          TextFormFieldNameWidget(
               controllers: nameController,
               hintTexting: "Enter Your Name",
               inputType: TextInputType.name),
           SizedBox(height: 10.h),
-          TextFormFieldWidget(
+          TextFormFieldEmailWidget(
               controllers: emailController,
               hintTexting: "Enter E-mail",
               inputType: TextInputType.emailAddress),
           SizedBox(height: 10.h),
-          TextFormFieldWidget(
+          TextFormFieldPhoneWidget(
               controllers: phoneNumController,
               hintTexting: "Enter Phone Number",
               inputType: TextInputType.phone),
-          //
-          /* TextField(
-            controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            style: const TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-                fillColor: Colors.black,
-                filled: true,
-                hintText: "Enter E-mail",
-                hintStyle: const TextStyle(color: Colors.white),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(2.w),
-                    borderSide: BorderSide(
-                        width: 0.1.h,
-                        color: Colors.purple,
-                        style: BorderStyle.solid)),
-                focusColor: Colors.yellow,
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(2.w),
-                    borderSide: BorderSide(
-                        width: 0.5.h,
-                        color: Colors.purple,
-                        style: BorderStyle.solid))),
-          ), */
-          /* SizedBox(height: 10.h),
-          TextField(
-            controller: phoneNumController,
-            keyboardType: TextInputType.phone,
-            style: const TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-                fillColor: Colors.black,
-                filled: true,
-                hintText: "Enter Phone Number",
-                hintStyle: const TextStyle(color: Colors.white),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(2.w),
-                    borderSide: BorderSide(
-                        width: 0.1.h,
-                        color: Colors.purple,
-                        style: BorderStyle.solid)),
-                focusColor: Colors.yellow,
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(2.w),
-                    borderSide: BorderSide(
-                        width: 0.5.h,
-                        color: Colors.purple,
-                        style: BorderStyle.solid))),
-          ), */
           SizedBox(height: 10.h),
           GestureDetector(
             onTap: () {
@@ -112,11 +64,11 @@ class ContactBook extends StatelessWidget {
   }
 }
 
-class TextFormFieldWidget extends StatelessWidget {
+class TextFormFieldEmailWidget extends StatelessWidget {
   final TextEditingController controllers;
   final String hintTexting;
   final TextInputType inputType;
-  const TextFormFieldWidget({
+  const TextFormFieldEmailWidget({
     Key? key,
     required this.controllers,
     required this.hintTexting,
@@ -127,6 +79,106 @@ class TextFormFieldWidget extends StatelessWidget {
     return TextFormField(
       controller: controllers, //
       keyboardType: inputType,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: ((value) {
+        if (!GetUtils.isEmail(value!)) {
+          return "Email is not valid";
+        } else {
+          return null;
+        }
+      }),
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+          fillColor: Colors.black,
+          filled: true,
+          hintText: hintTexting,
+          hintStyle: const TextStyle(color: Colors.white),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(2.w),
+              borderSide: BorderSide(
+                  width: 0.1.h,
+                  color: Colors.purple,
+                  style: BorderStyle.solid)),
+          focusColor: Colors.yellow,
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(2.w),
+              borderSide: BorderSide(
+                  width: 0.5.h,
+                  color: Colors.purple,
+                  style: BorderStyle.solid))),
+    );
+  }
+}
+
+class TextFormFieldPhoneWidget extends StatelessWidget {
+  final TextEditingController controllers;
+  final String hintTexting;
+  final TextInputType inputType;
+  const TextFormFieldPhoneWidget({
+    Key? key,
+    required this.controllers,
+    required this.hintTexting,
+    required this.inputType,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controllers, //
+      keyboardType: inputType,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: ((value) {
+        if (!GetUtils.isPhoneNumber(value!)) {
+          return "Phone is not valid";
+        } else {
+          return null;
+        }
+      }),
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+          fillColor: Colors.black,
+          filled: true,
+          hintText: hintTexting,
+          hintStyle: const TextStyle(color: Colors.white),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(2.w),
+              borderSide: BorderSide(
+                  width: 0.1.h,
+                  color: Colors.purple,
+                  style: BorderStyle.solid)),
+          focusColor: Colors.yellow,
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(2.w),
+              borderSide: BorderSide(
+                  width: 0.5.h,
+                  color: Colors.purple,
+                  style: BorderStyle.solid))),
+    );
+  }
+}
+
+class TextFormFieldNameWidget extends StatelessWidget {
+  final TextEditingController controllers;
+  final String hintTexting;
+  final TextInputType inputType;
+  const TextFormFieldNameWidget({
+    Key? key,
+    required this.controllers,
+    required this.hintTexting,
+    required this.inputType,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controllers, //
+      keyboardType: inputType,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: ((value) {
+        if (!GetUtils.isUsername(value!)) {
+          return "Username is not valid";
+        } else {
+          return null;
+        }
+      }),
       style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
           fillColor: Colors.black,
